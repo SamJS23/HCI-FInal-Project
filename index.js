4be2e0eb7147baa8c2b83daa2e5845d839ab86f0
SamJS23 <samuel23505@gmail.com>
2023-12-13 08:37:40 +0700

    commit

--- index.js
+++ index.js
@@ -25,61 +25,9 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 window.addEventListener('scroll', function () {
-  var content = document.querySelector('#first');
-  var position = content.getBoundingClientRect().top;
-  if (position < window.innerHeight) {
-      content.classList.add('fadeLeft');
-  }
-});
-window.addEventListener('scroll', function () {
-  var content = document.querySelector('#second');
-  var position = content.getBoundingClientRect().top;
-  if (position < window.innerHeight - 50 ) {
-      content.classList.add('fadeRight');
-  }
-});
-window.addEventListener('scroll', function () {
-  var content = document.querySelector('#third');
-  var position = content.getBoundingClientRect().top;
-  if (position < window.innerHeight - 65 ) {
-      content.classList.add('fadeLeft');
-  }
-});
-window.addEventListener('scroll', function () {
-  var content = document.querySelector('#fourth');
-  var position = content.getBoundingClientRect().top;
-  if (position < window.innerHeight - 65 ) {
-      content.classList.add('fadeRight');
-  }
-});
-window.addEventListener('scroll', function () {
   var content = document.querySelector('#main1');
   var position = content.getBoundingClientRect().top;
   if (position < window.innerHeight - 70) {
       content.classList.add('fadeIn');
   }
-});
-document.querySelector('#togglePopup1').addEventListener("click", function(){
-  document.querySelector("#popup1").classList.add("active");
-});
-document.querySelector('#close1').addEventListener("click", function(){
-  document.querySelector("#popup1").classList.remove("active");
-});
-document.querySelector('#togglePopup2').addEventListener("click", function(){
-  document.querySelector("#popup2").classList.add("active");
-});
-document.querySelector('#close2').addEventListener("click", function(){
-  document.querySelector("#popup2").classList.remove("active");
-});
-document.querySelector('#togglePopup3').addEventListener("click", function(){
-  document.querySelector("#popup3").classList.add("active");
-});
-document.querySelector('#close3').addEventListener("click", function(){
-  document.querySelector("#popup3").classList.remove("active");
-});
-document.querySelector('#togglePopup4').addEventListener("click", function(){
-  document.querySelector("#popup4").classList.add("active");
-});
-document.querySelector('#close4').addEventListener("click", function(){
-  document.querySelector("#popup4").classList.remove("active");
 });
---
+++ rooms.js
@@ -0,0 +1,52 @@
+window.addEventListener('scroll', function () {
+  var content = document.querySelector('#first');
+  var position = content.getBoundingClientRect().top;
+  if (position < window.innerHeight) {
+      content.classList.add('fadeLeft');
+  }
+});
+window.addEventListener('scroll', function () {
+  var content = document.querySelector('#second');
+  var position = content.getBoundingClientRect().top;
+  if (position < window.innerHeight - 55 ) {
+      content.classList.add('fadeRight');
+  }
+});
+window.addEventListener('scroll', function () {
+  var content = document.querySelector('#third');
+  var position = content.getBoundingClientRect().top;
+  if (position < window.innerHeight - 65 ) {
+      content.classList.add('fadeLeft');
+  }
+});
+window.addEventListener('scroll', function () {
+  var content = document.querySelector('#fourth');
+  var position = content.getBoundingClientRect().top;
+  if (position < window.innerHeight - 65 ) {
+      content.classList.add('fadeRight');
+  }
+});
+document.querySelector('#togglePopup1').addEventListener("click", function(){
+  document.querySelector("#popup1").classList.add("active");
+});
+document.querySelector('#close1').addEventListener("click", function(){
+  document.querySelector("#popup1").classList.remove("active");
+});
+document.querySelector('#togglePopup2').addEventListener("click", function(){
+  document.querySelector("#popup2").classList.add("active");
+});
+document.querySelector('#close2').addEventListener("click", function(){
+  document.querySelector("#popup2").classList.remove("active");
+});
+document.querySelector('#togglePopup3').addEventListener("click", function(){
+  document.querySelector("#popup3").classList.add("active");
+});
+document.querySelector('#close3').addEventListener("click", function(){
+  document.querySelector("#popup3").classList.remove("active");
+});
+document.querySelector('#togglePopup4').addEventListener("click", function(){
+  document.querySelector("#popup4").classList.add("active");
+});
+document.querySelector('#close4').addEventListener("click", function(){
+  document.querySelector("#popup4").classList.remove("active");
+});
