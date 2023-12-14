320c62db8ec2bd388bd403d78736b0cb51773e65
SamJS23 <samuel23505@gmail.com>
2023-12-14 23:21:31 +0700

    image gallery

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+  
+}
+let Index = 1;
+showS(Index);
+
+function plusS(n) {
+  showS(Index += n);
+}
+
+function currentS(n) {
+  showS(Index = n);
+}
+
+function showS(n) {
+  let i;
+  let s = document.getElementsByClassName("myS");
+  let d = document.getElementsByClassName("d");
+  if (n > s.length) {Index = 1}    
+  if (n < 1) {Index = s.length}
+  for (i = 0; i < s.length; i++) {
+    s[i].style.display = "none";  
+  }
+  for (i = 0; i < d.length; i++) {
+    d[i].className = d[i].className.replace(" active", "");
+  }
+  s[Index-1].style.display = "block";  
+  d[Index-1].className += " active";
+  
 }
 window.addEventListener('scroll', function () {
   var content = document.querySelector('#main1');
